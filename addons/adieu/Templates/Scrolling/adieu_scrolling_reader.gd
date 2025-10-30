667a08f712af4178212430fe06fcd134cca03857
Sebastian Alkstrand <sebastian.alkstrand@gmail.com>
2025-10-30 23:21:48 +0100

    Added basic functionality for scrolling credits, now also supporting styling tags: min-height, gap, text-align and text-color.

--- addons/adieu/Templates/Scrolling/adieu_scrolling_reader.gd
+++ addons/adieu/Templates/Scrolling/adieu_scrolling_reader.gd
@@ -1,9 +1,17 @@
 extends Control
 
 var built: bool = false;
+var running: bool = false;
+var sped_up: bool = false;
+var current_offset: float = 0;
+var last_context: String = "";
 
 @export var credits: AdieuResource;
 @export var autoplay: bool = true;
+
+@export_custom(PROPERTY_HINT_NONE, "suffix:px/s") var scroll_speed: float = 12;
+@export var speed_up_multiplier: float = 3.0;
+@export var speed_up_action: InputEventAction;
 
 @export_subgroup("Margins")
 @export var include_top_y_in_start: bool = false;
@@ -15,7 +23,7 @@ var built: bool = false;
 @export var context_font: Font;
 @export var text_font: Font;
 @export var default_styling: Dictionary[String, String] = {};
-@export var skip_repeat_contexts: bool = true;
+@export var hide_repeat_contexts: bool = true;
 
 @export_subgroup("Components")
 @export var margin_container: MarginContainer;
@@ -24,10 +32,26 @@ var built: bool = false;
 
 func _ready() -> void:
 	_apply_margins();
-	#_apply_initial_offset();
+	_apply_initial_offset();
 
 	if autoplay:
 		run();
+
+func _process(delta: float) -> void:
+	sped_up = Input.is_action_pressed(speed_up_action.action);
+
+	if running:
+		_scroll_step(delta);
+
+func build():
+	if credits == null:
+		push_error("[ADIEU] Failed to build credits, no resource was present.");
+		return;
+
+	for section in credits.data["Sections"]:
+		_build_section(section, list_container);
+
+	built = true;
 
 func run():
 	if credits == null:
@@ -37,16 +61,23 @@ func run():
 	if !built:
 		build();
 
-	# Do shit!
-	pass;
+	running = true;
 
-func build():
-	if credits == null:
-		push_error("[ADIEU] Failed to build credits, no resource was present.");
-		return;
+func stop():
+	running = false;
 
-	for section in credits.data["Sections"]:
-		_build_section(section, list_container);
+func _scroll_step(delta: float):
+	var base_offset = _get_base_offset();
+	var diff = (scroll_speed * delta * _get_speed_mult());
+
+	panel_container.global_position.y -= diff;
+	current_offset -= diff;
+
+	if panel_container.global_position.y - (base_offset + current_offset) > 12:
+		panel_container.global_position.y = (base_offset + current_offset);
+
+func _get_speed_mult() -> float:
+	return speed_up_multiplier if sped_up else 1.0;
 
 # --- Initialization ---
 
@@ -58,10 +89,14 @@ func _apply_margins():
 
 func _apply_initial_offset():
 	var viewport_height = get_viewport_rect().size.y;
+	panel_container.global_position.y = viewport_height;
 
-	panel_container.global_position.y += viewport_height;
+func _get_base_offset() -> float:
+	var total_h = get_viewport_rect().size.y;
 	if include_top_y_in_start:
-		panel_container.global_position.y -= credits_y_margins.x;
+		total_h -= credits_y_margins.x;
+
+	return total_h;
 
 # --- Prop Builders ---
 
@@ -69,11 +104,21 @@ func _build_section(section: Dictionary, list_parent: VBoxContainer):
 	var dup_default_styling = default_styling.duplicate();
 	var section_style = section["Styling"].duplicate();
 
-	section_style.merge(dup_default_styling);
+	section_style.merge(dup_default_styling, false);
+	last_context = ""; # Clear last context for each section to prevent false positives
 
 	var section_container = VBoxContainer.new();
+	section_container.name = section["Title"];
+
 	if AdieuStyling.should_display_header(section_style):
 		section_container.add_child(_build_section_header(section["Title"], section_style));
+
+	for line in section["Lines"]:
+		section_container.add_child(_build_section_line(line, section_style));
+
+	for style_key in section_style.keys():
+		match style_key:
+			AdieuStyling.KEYS.MIN_HEIGHT: AdieuStyling.apply_min_height(section_style[style_key], section_container);
 
 	list_parent.add_child(section_container);
 
@@ -92,6 +137,70 @@ func _build_section_header(header: String, styling: Dictionary) -> Control:
 			AdieuStyling.KEYS.HEADER_ALIGN: AdieuStyling.apply_text_align(styling[style_key], label);
 			AdieuStyling.KEYS.HEADER_SIZE: AdieuStyling.apply_text_size(styling[style_key], label);
 			AdieuStyling.KEYS.HEADER_COLOR: AdieuStyling.apply_text_color(styling[style_key], label);
-		pass;
+
+	return label;
+
+func _build_section_line(line: Dictionary, styling: Dictionary) -> Control:
+	var content_container = HBoxContainer.new();
+	content_container.alignment = BoxContainer.ALIGNMENT_CENTER;
+
+	content_container.add_child(_build_section_line_text(line, styling));
+	if AdieuStyling.should_display_context(styling):
+		content_container.add_child(_build_section_line_context(line, styling));
+
+	for style_key in styling.keys():
+		match style_key:
+			AdieuStyling.KEYS.GAP: AdieuStyling.apply_gap(styling[style_key], content_container);
+
+	return content_container;
+
+func _build_section_line_text(line: Dictionary, styling: Dictionary) -> Control:
+	var label = Label.new();
+	var label_settings = LabelSettings.new();
+
+	label.text = line["Text"];
+
+	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART;
+	label.size_flags_vertical   = Control.SIZE_FILL;
+	label.size_flags_horizontal = Control.SIZE_EXPAND_FILL;
+
+	label.label_settings = label_settings;
+
+	if text_font != null:
+		label.label_settings.font = text_font;
+
+	for style_key in styling.keys():
+		match style_key:
+			AdieuStyling.KEYS.TEXT_ALIGN: AdieuStyling.apply_text_align(styling[style_key], label);
+			AdieuStyling.KEYS.TEXT_SIZE: AdieuStyling.apply_text_size(styling[style_key], label);
+			AdieuStyling.KEYS.TEXT_COLOR: AdieuStyling.apply_text_color(styling[style_key], label);
+
+	return label;
+
+func _build_section_line_context(line: Dictionary, styling: Dictionary) -> Control:
+	var label = Label.new();
+	var label_settings = LabelSettings.new();
+
+	var context = line.get("Context", "");
+
+	if context == last_context && hide_repeat_contexts:
+		label.text = "";
+	else:
+		label.text = context;
+		last_context = context;
+
+	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART;
+	label.size_flags_vertical   = Control.SIZE_FILL;
+	label.size_flags_horizontal = Control.SIZE_EXPAND_FILL;
+	label.label_settings = label_settings;
+
+	if context_font != null:
+		label.label_settings.font = context_font;
+
+	for style_key in styling.keys():
+		match style_key:
+			AdieuStyling.KEYS.CONTEXT_ALIGN: AdieuStyling.apply_text_align(styling[style_key], label);
+			AdieuStyling.KEYS.CONTEXT_SIZE: AdieuStyling.apply_text_size(styling[style_key], label);
+			AdieuStyling.KEYS.CONTEXT_COLOR: AdieuStyling.apply_text_color(styling[style_key], label);
 
 	return label;
--- addons/adieu/Utils/styling_utils.gd
+++ addons/adieu/Utils/styling_utils.gd
@@ -1,15 +1,30 @@
 class_name AdieuStyling;
 
 const KEYS = {
+	"EVENT_ON": "event-on", # 'Enter', 'Middle', 'Exit'
 	"DISPLAY_SECTION_HEADER": "display-section-header",
+	"DISPLAY_SECTION_CONTEXT": "display-section-context",
+	"MIN_HEIGHT": "min-height",
+	"GAP": "gap",
 	"HEADER_ALIGN": "header-align", # internally calls same as text-align
+	"CONTEXT_ALIGN": "context-align", # internally calls same as text-align
+	"TEXT_ALIGN": "text-align",
 	"HEADER_SIZE": "header-size", # internally calls same as text-size
-	"HEADER_COLOR": "header-color" # internally calls same as text-color
+	"CONTEXT_SIZE": "context-size", # internally calls same as text-size
+	"TEXT_SIZE": "text-size",
+	"HEADER_COLOR": "header-color", # internally calls same as text-color
+	"CONTEXT_COLOR": "context-color", # internally calls same as text-color
+	"TEXT_COLOR": "text-color",
 };
 
 static func should_display_header(styling: Dictionary) -> bool:
 	if styling.has(KEYS.DISPLAY_SECTION_HEADER):
 		return styling.get(KEYS.DISPLAY_SECTION_HEADER).to_lower() == "true";
+	return true;
+
+static func should_display_context(styling: Dictionary) -> bool:
+	if styling.has(KEYS.DISPLAY_SECTION_CONTEXT):
+		return styling.get(KEYS.DISPLAY_SECTION_CONTEXT).to_lower() == "true";
 	return true;
 
 static func apply_text_align(value: String, label: Label):
@@ -28,3 +43,13 @@ static func apply_text_color(value: String, label: Label):
 	if value.is_valid_html_color():
 		var col = Color.from_string(value, Color.WHITE);
 		label.label_settings.font_color = col;
+
+static func apply_min_height(value: String, control: Control):
+	if value.is_valid_int():
+		var intified = value.to_int();
+		control.custom_minimum_size.y = intified;
+
+static func apply_gap(value: String, box_container: BoxContainer):
+	if value.is_valid_int():
+		var intified = value.to_int();
+		box_container.add_theme_constant_override("separation", intified);
